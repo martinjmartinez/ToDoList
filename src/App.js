f63a2cba3acab06d3c1e26fba781213660636c04
Martin J Martinez <martinjmartinezcruz@gmail.com>
2018-05-10 14:01:33 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import  React, { Component } from 'react';
+import  {DB_CONFIG} from './Config/config';
+import  Note from './Note/Note';
+import  NoteForm from './NoteForm/NoteForm';
+import  firebase from 'firebase/app';
+import 'firebase/database';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.addNote = this.addNote.bind(this);
+        this.removeNote = this.removeNote.bind(this);
+
+
+        this.app = firebase.initializeApp(DB_CONFIG);
+        this.db = this.app.database().ref().child('notes');
+
+        this.state = {
+            notes: [],
+        }
+    }
+
+    componentWillMount() {
+        const previousNotes  = this.state.notes;
+
+        this.db.on('child_added', snap => {
+            previousNotes.push({
+                id: snap.key,
+                noteContent: snap.val().noteContent,
+            })
+
+            this.setState({
+                notes: previousNotes
+            })
+        })
+
+        this.db.on('child_removed', snap => {
+            for (let i = 0; i < previousNotes.length; i++) {
+                if(previousNotes[i].id === snap.key) {
+                    previousNotes.splice(i, 1);
+                }
+            }
+
+            this.setState({
+                notes: previousNotes
+            })
+        })
+    }
+
+    addNote(note) {
+        this.db.push().set({ noteContent: note});
+    }
+
+    removeNote(noteId) {
+        console.log(noteId);
+        this.db.child(noteId).remove();
+    }
+
+  render() {
+    return (
+     <div className="notesWrapper">
+         <div className="notesHeader">
+             <div className="heading">React & Firebase To-Do List </div>
+         </div>
+         <div className="notesBody">
+             {
+                 this.state.notes.map((note) => {
+                     return (
+                         <Note noteContent={note.noteContent}
+                                   noteId={note.id}
+                                   key={note.id}
+                                   removeNote={this.removeNote}
+                         />)
+                 })
+             }
+         </div>
+         <div className="notesFooter">
+             <NoteForm addNote={this.addNote}/>
+         </div>
+     </div>
+    );
+  }
+}
+
+export default App;
